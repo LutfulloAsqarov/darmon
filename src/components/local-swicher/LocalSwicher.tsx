73e9cd0634583315b504b28e324be907abbf953c
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-07-30 09:54:10 +0500

    update

--- src/components/local-swicher/LocalSwicher.tsx
+++ src/components/local-swicher/LocalSwicher.tsx
@@ -9,19 +9,25 @@ import Image from "next/image";
 const LocalSwitcher: React.FC = () => {
     const router = useRouter();
     const path = usePathname();
-    const [lang, setLang] = useState<string>(() => {
-        return localStorage.getItem("lang") || "uz";
-    });
+    let l: string | null = "uz";
+    if (typeof window !== "undefined") {
+        l = localStorage.getItem("lang");
+    }
+
+    const [lang, setLang] = useState<null | string>(l);
 
     useEffect(() => {
-        localStorage.setItem("lang", lang);
+        localStorage.setItem("lang", lang || "uz");
         const pathname = path.split("/").slice(2).join("/");
         router.replace(`/${lang}/${pathname}`);
     }, [lang, path, router]);
 
     return (
         <div className="header__len">
-            <select value={lang} onChange={(e) => setLang(e.target.value)}>
+            <select
+                value={lang || "uz"}
+                onChange={(e) => setLang(e.target.value)}
+            >
                 <option value="uz">UZ</option>
                 <option value="en">EN</option>
                 <option value="ru">RU</option>
